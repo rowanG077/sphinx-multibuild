bfcbe8138f01ae52eea006dcd7621e155641ee69
rowanG077 <goemansrowan@gmail.com>
2018-04-05 10:53:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@
 from distutils.core import setup
 setup(
   name = 'sphinx-multibuild',
-  packages = ['sphinx-multibuild'], # this must be the same as the name above
+  packages = ['sphinx-multibuild'],
   version = '0.3',
   description = 'Allow sphinx to build with multiple source directories and watch for changes.',
   author = 'Rowan Goemans',
   author_email = 'goemansrowan@gmail.com',
-  url = 'https://github.com/rowanG077/sphinx-multibuild', # use the URL to the github repo
-  download_url = 'https://github.com/rowanG077/sphinx-multibuild/archive/0.3.tar.gz', # I'll explain this in a second
-  keywords = ['sphinx', 'autobuild', 'multiple-directories'], # arbitrary keywords
+  url = 'https://github.com/rowanG077/sphinx-multibuild',
+  download_url = 'https://github.com/rowanG077/sphinx-multibuild/archive/0.3.tar.gz', 
+  keywords = ['sphinx', 'autobuild', 'multiple-directories'],
   classifiers = [],
 )
