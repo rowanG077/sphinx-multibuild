d21363d3e37a25c41dbb5c3441bdf6b201d52477
Melvin Vermeeren <mail@mel.vin>
2018-05-04 19:27:16 +0200

    fix compatibility with py3 when installed as system module

--- sphinx_multibuild/__init__.py
+++ sphinx_multibuild/__init__.py
@@ -1,3 +1,4 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
-from sphinx_multibuild import SphinxMultiBuilder
+from __future__ import absolute_import
+from .sphinx_multibuild import SphinxMultiBuilder
